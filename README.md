3a0829dc6d1c445df32c41eb2f32ea7e0e780a6a
fatihcimen <fatih@fatihcimen.com>
2021-05-14 00:42:13 +0300

    [Xcodeproj] - Implement Xcodeproj

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # FCExtensionKit
 
-[![Build Status](https://www.travis-ci.com/fatihcimen/FCExtensionKit.svg?branch=main)](https://www.travis-ci.com/fatihcimen/FCExtensionKit)
-
-A little helper library for Swift.
+A description of this package.
---
+++ Sources/FCExtensionKit.h
@@ -0,0 +1,18 @@
+//
+//  FCExtensionKit.h
+//  FCExtensionKit
+//
+//  Created by Fatih Çimen on 13.05.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FCExtensionKit.
+FOUNDATION_EXPORT double FCExtensionKitVersionNumber;
+
+//! Project version string for FCExtensionKit.
+FOUNDATION_EXPORT const unsigned char FCExtensionKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FCExtensionKit/PublicHeader.h>
+
+#import <FCExtensionKit/CGImage+WebP.h>
